f4ae3f9cb13e440018ed93fb71e2ea5c66218d3b
Juan David Bolivar <juan.bolivar@docctechno.com>
2024-09-15 20:33:04 -0500

    Subide la prueba tecnica

--- config/app.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const APP_URL = "http://localhost/prueba-pana/";
-const APP_NAME = "prueba pana business";
-const APP_SESSION_NAME = "prueba";
-date_default_timezone_set("America/Bogota");
--- config/server.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-const DB_SERVER = "localhost";
-const DB_NAME = "prueba_pana";
-const DB_USER = "root";
-const DB_PASSWORD = "";
